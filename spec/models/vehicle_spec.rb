1e19506784a03322b612a335b58232355cc23651
Johan Tinjaca <stiven10@gmail.com>
2021-05-29 09:32:32 -0500

    Add Vehicle tests

--- spec/models/vehicle_spec.rb
+++ spec/models/vehicle_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Vehicle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'associations' do
+    it 'should respond with' do
+      is_expected.to belong_to(:vehicle_model)
+    end
+  end
+
+  it { is_expected.to validate_presence_of(:mileage) }
+  it { is_expected.to validate_presence_of(:year) }
+  it { is_expected.to validate_presence_of(:price) }
 end
