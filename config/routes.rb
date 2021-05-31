f137690013667c95fcb27b45561849e8a3ed816d
Johan Tinjaca <stiven10@gmail.com>
2021-05-30 19:10:25 -0500

    Add route post create vehicle

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :vehicle_models, only: %i[create]
+      resources :vehicles, only: %i[create]
     end
   end
 end
